b43de1a5f0e1eb6ad992d4100533b3aa248667b1
Elvin Zhang <zhangweicalm@gmail.com>
2024-04-23 11:54:54 +0800

    add crawl opensubtitles

--- pytool/scripts/crawl_opensubtitles_resolve.py
+++ pytool/scripts/crawl_opensubtitles_resolve.py
@@ -13,11 +13,20 @@ r = redis.Redis(host='192.168.2.203', port=6379, db=0,decode_responses=True)
 cwd = os.getcwd()
 print(cwd)
 import sys
-sys.path.append(cwd+"\\..")
+sys.path.append(cwd)
 import request
 
 
 FILE_PATH_PREFIX = cwd+'\\scripts\\file\\opensubtitles\\'
+#FILE_PATH_PREFIX = r'\\192.168.2.201\\d\\opensubs\\'
+#FILE_PATH_PREFIX = r'd:/opensubs/'
+
+
+def get_proxy():
+    return requests.get("http://127.0.0.1:5010/get/?type=https").json()
+
+def delete_proxy(proxy):
+    requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
 
 def increment_counter():  
     # 使用INCR命令递增my_counter的值  
@@ -28,38 +37,106 @@ def increment_counter():
     return new_value
 
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+download_dir = FILE_PATH_PREFIX
+chrome_options.add_experimental_option('prefs', {
+    "download.default_directory": download_dir,
+    #"download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True,
+    'window-size':'300,300'
+})
+chrome_options.add_argument('blink-settings=imagesEnabled=false')
+chrome_options.add_argument('window-size=300,300')
+import time
+proxy_count =5
+useProxy = False
+def download_file_with_selenium(url):
+    global id, proxy, proxy_count
+    if useProxy:
+        if proxy is None:
+            proxy = get_proxy().get("proxy")
+            print(f"Using proxy {proxy}")
+        proxy_count -= 1
+        if proxy_count < 0:
+            delete_proxy(proxy)
+            proxy = get_proxy().get("proxy")
+            print(f"Using proxy {proxy}")
+            proxy_count = 20
+    chrome_options.add_argument("--proxy-server=%s" % proxy) 
+    driver = webdriver.Chrome(options=chrome_options)
+    try:
+        #url = 'https://www.baidu.com'
+        driver.get(url)
+        wait_time = 10
+        while wait_time > 0 :
+            time.sleep(2)
+            for root, dirs, files in os.walk(FILE_PATH_PREFIX):  
+                # 对于每个文件，检查其后缀是否为所需的后缀  
+                for file in files:  
+                    if file.endswith('.zip'):
+                        file_path = os.path.join(root, file)  
+                        return file_path, file 
+            wait_time -= 1
+        return None, None
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit() 
+        
+proxy = None
 def download_file_from_url(url):  
+    global proxy_count, proxy
+    proxy_count -= 1
     local_path = None
-    try:  
-        response = requests.get(url,allow_redirects=True ,stream=True)  
-        response.raise_for_status()  # 再次检查GET请求的状态  
-        filename = None  
-        content_disposition = response.headers.get('Content-Disposition')  
-        if content_disposition:  
-            _, params = cgi.parse_header(content_disposition)  
-            filename = params.get('filename')    
-        if not filename:  
-            # 如果Content-Disposition中没有文件名，则使用URL的最后一部分作为文件名  
-            filename = os.path.basename(urlparse(url).path)  
-       
-        # 构造完整的本地文件路径  
-        local_filename = filename  
-        local_path = os.path.join(FILE_PATH_PREFIX, local_filename)    
-        with open(local_path, 'wb') as f:  
-            for chunk in response.iter_content(chunk_size=8192):  
-                if chunk:  # 过滤掉空的chunks  
-                    f.write(chunk)  
-    except requests.RequestException as e:  
-        if os.path.exists(local_path):  
-            os.remove(local_path)  # 如果文件已经部分下载，删除它  
-        raise ValueError(f"Failed to download file from {url}. Error: {e}")  
-    except IOError as e:  
-        if os.path.exists(local_path):  
-            os.remove(local_path)  # 如果文件已经部分写入，删除它  
-        raise ValueError(f"Failed to write file to {local_path}. Error: {e}")  
-      
+    local_filename = None
+    if proxy is None:
+        proxy = get_proxy().get("proxy")
+    if proxy_count < 0 :
+        delete_proxy(proxy)
+        proxy = get_proxy().get("proxy")
+    print(f"Using proxy {proxy}")
+    retry_count = 1
+    while retry_count <= 3 :
+        try:  
+            print(f"retry {retry_count}")
+            retry_count += 1
+            proxy = {"http": "http://{}".format(proxy),"http": "http://{}".format(proxy)},
+            #response = requests.get(url,allow_redirects=True ,stream=True,proxies={"http": "http://{}".format(proxy),"https": "http://{}".format(proxy)}, timeout=30)  
+            response = requests.get(url,allow_redirects=True ,stream=True,proxies= timeout=30)  
+
+            if response.status_code == 404:
+                return '404', None
+            response.raise_for_status()  # 再次检查GET请求的状态  
+            filename = None  
+            content_disposition = response.headers.get('Content-Disposition')  
+            if content_disposition:  
+                _, params = cgi.parse_header(content_disposition)  
+                filename = params.get('filename')    
+            if not filename:  
+                # 如果Content-Disposition中没有文件名，则使用URL的最后一部分作为文件名  
+                filename = os.path.basename(urlparse(url).path)  
+        
+            # 构造完整的本地文件路径  
+            local_filename = filename  
+            local_path = os.path.join(FILE_PATH_PREFIX, local_filename)    
+            with open(local_path, 'wb') as f:  
+                for chunk in response.iter_content(chunk_size=8192):  
+                    if chunk:  # 过滤掉空的chunks  
+                        f.write(chunk) 
+            return local_path,  local_filename
+        except Exception as e:
+            if local_path and os.path.exists(local_path):
+                os.remove(local_path)  # 如果文件已经部分写入，删除它
+            print(f"Failed to download file from {url}. Error: {e}")
+            delete_proxy(proxy)
+            proxy = get_proxy().get("proxy")
+            print(f"Using proxy {proxy}")
+    return None, None
     #print(f"File {filename} downloaded successfully.")  
-    return local_path,  local_filename
+    
 
 def unzip_file(zip_filepath, dest_path):  
     with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:  
@@ -103,6 +180,8 @@ def find_files_without_suffix(suffix, path='.'):
 
 def parse_nfo_file(nfo_path):  
     line_count = 0 
+    pattern_name = re.compile(r'^Û\s+(.*\s+subtitles)\s+Û$')
+    line_count_name = 10000
     pattern_movie_info_start = re.compile(r'^Û\s+MOVIE INFORMATION\s+Û$') 
     line_count_movie_info_start = 10000
     pattern_subtitle_info_start = re.compile(r'^Û\s+SUBTITLE INFORMATION\s+Û$') 
@@ -111,14 +190,22 @@ def parse_nfo_file(nfo_path):
     current_cd_count = 0
     line_count_current_cd = 10000
     keywords = ['Link', 'Filename', 'Language', 'Format', 'Total', 'Release Name']  
-    movie_info = {}  
+    movie_info = {'Name':'','Link':'', 'Language':'', 'Filename':'', 'Format':'', 'Total':'', 'Release Name':''}  
 
     with open(nfo_path, 'r', encoding='utf-8') as nfo_file:  
         for line in nfo_file:
             line_count = line_count + 1
+
+            if line_count < line_count_name:
+                match = pattern_name.search(line)
+                if match:
+                    movie_info['Name'] = match.group(1).strip()
+                    line_count_name = line_count
+
             if re.match(pattern_subtitle_info_start, line):#获取subtitle info开始行数
                 line_count_subtitle_info_start = line_count 
                 #print("found line_count_subtitle_info_start",line_count_subtitle_info_start)
+
             if re.match(pattern_cd_info_start, line):#获取cd info开始行数
                 line_count_current_cd = line_count
                 #print("found line_count_current_cd",line_count_current_cd)
@@ -135,17 +222,35 @@ id = 0
 while True:
     id = increment_counter()
     print("dealing "+str(id))
-    if id > 7118651 :
+    if id > 10000000 :
         break
+    if r.sismember('opensubs_crawl_dealed', id):
+        continue
     try:
         #https://www.opensubtitles.org/en/subtitleserve/sub/9718896
-        url_base = r"https://www.opensubtitles.org/en/subtitleserve/sub/"
-        url_base1 = r"https://dl.opensubtitles.org/en/download/sub/"
+        #url_base = r"https://www.opensubtitles.org/en/subtitleserve/sub/"
+        url_base = r"https://dl.opensubtitles.org/en/download/sub/"
 
         url = url_base + str(id)
 
         #下载
-        local_path, local_filename = download_file_from_url(url)
+        retry_count = 5
+        local_path = None
+        local_filename = None
+        while retry_count >0 :
+            local_path, local_filename = download_file_from_url(url)
+            #local_path, local_filename = download_file_with_selenium(url)
+            if local_path is not None:
+                break   
+            retry_count -= 1
+
+        if local_path == '404':
+            r.sadd('opensubs_crawl_dealed', id)
+            print("id "+str(id) + "失败  404")
+            continue
+        
+        if local_path is None:
+            raise Exception("id"+str(id)+"下载失败")
         #local_path = FILE_PATH_PREFIX+"alien3.(1992).eng.2cd.(1).zip"
 
         #解压缩
@@ -166,10 +271,12 @@ while True:
         subtitles = '&&&&'.join(subtitle)
 
         #存库
-        params = [id, movie_info['Link'],  movie_info['Language'], movie_info['Filename'], movie_info['Format'], movie_info['Total'], movie_info['Release Name'], subtitles]
+        params = [str(id), movie_info['Name'] ,movie_info['Link'],  movie_info['Language'], movie_info['Filename'], movie_info['Format'], movie_info['Total'], movie_info['Release Name'], subtitles]
         result = request.remote_call('p_save_opensubtitles',params)
-        if result.rc != '000':
-            print(result.rc, result.msg)
+        if result['rc'] != '000':
+            print(result['rc'], result['rm'])
+            continue
+        r.sadd('opensubs_crawl_dealed', id)
 
     except Exception as e:
         print("id "+str(id) + "失败")
